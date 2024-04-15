1fd6735a99a00650c2080e22e9b1b7c343196e31
jokudev <git@joku.dev>
2024-04-15 15:05:45 +0200

    add SecurityConfig

---
+++ src/main/java/ch/kunz/joshua/flugbuchungsapi/security/Roles.java
@@ -0,0 +1,8 @@
+package ch.kunz.joshua.flugbuchungsapi.security;
+
+public class Roles {
+    public static final String User  = "user";
+    public static final String Staff = "staff";
+    public static final String Admin = "admin";
+}
+
